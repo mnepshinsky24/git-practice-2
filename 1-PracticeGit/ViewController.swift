4753f06f2c5a6ca1f7787ef27f651df0e0328797
Megan Nepshinsky <megan@rmp05-mbair13-12-6834-megan-nepshinsky.local>
2019-05-17 15:23:52 -0600

    Initial Commit

---
+++ 1-PracticeGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1-PracticeGit
+//
+//  Created by Megan Nepshinsky on 5/17/19.
+//  Copyright © 2019 Megan Nepshinsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
